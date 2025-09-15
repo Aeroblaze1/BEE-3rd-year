359685826d7e43b25ae2c0b653101b50d4cf916c
Aeroblaze1 <siddhantb610@gmail.com>
2025-09-15 15:45:51 +0530

    PRISMA WORKING

---
+++ LEC-28/index.js
@@ -0,0 +1,28 @@
+const {PrismaClient} = require('./generated/prisma')
+
+const prisma = new PrismaClient();
+
+async function addUser(email,name,password){
+     await prisma.user.create({
+        data:{
+            email:email,
+            name:name,
+            password:password
+        }
+     })
+}
+
+addUser("siddhantb610@gmail.com","Siddhant","1234")
+.then(()=>{
+    console.log("user added successfully");
+})
+
+async function getAllUser(){
+    let allUser = await prisma.user.findMany();
+    return allUser;
+}
+
+getAllUser().
+then((data)=>{
+    console.log(data);
+})
